0cd28f8d01cf064cfb33429d31954f94e8af4ba8
Jhantelle Belleza <jhantellebelleza@Jhantelles-MacBook-Pro.local>
2016-08-29 17:08:31 -0400

    Completed Program

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -21,7 +21,58 @@ class ViewController: UIViewController {
     
     
     // Create your methods here
+    
+    // Exercise 1
+    func addNameToDeliLine(name:String)->String{
+        let firstName = name.componentsSeparatedByString(" ")
+        var message = ""
+        
+        if deliLine.isEmpty{
+            deliLine.append(name)
+            message = "Welcome \(name), you're first in line!"
 
-
+        }
+        else if name == "Billy Crystal" ||
+                name == "Meg Ryan"{
+           deliLine.insert(name, atIndex: 0)
+           message = "Welcome \(firstName[0])! You can sit wherever you like."
+           print(deliLine)
+            }
+        
+        else{
+           deliLine.append(name)
+           message = "Welcome \(name), you're number \(deliLine.endIndex) in line."
+        }
+        
+        return message }
+        
+    // Exercise 2
+    func nowServing()->String{
+        var message = ""
+    if deliLine.isEmpty{
+       message = "There is no-one to be served."
+    }else{
+     for (index, name) in deliLine.enumerate(){
+       message = "Now serving \(name)"
+        deliLine.removeAtIndex(index)}
+    }
+        return message
+    }
+    
+    //Exercise 3
+    func deliLineDescription()->String{
+        var message = ""
+        if deliLine.isEmpty{
+            message = "The line is currently empty."
+        }else{
+            for (index, name) in deliLine.enumerate(){
+                message = "The line is:\n"
+                message = message + ("\(index) \(name)")
+        }
+            
+          //  _ = arc4random_uniform(20)
+        
+        }
+            return message
 }
-
+}
